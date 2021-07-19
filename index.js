4287da26ed6ebe23fa2bc23c360810d708f47198
Yaroslava Yemets <yaroslava1058@gmail.com>
2021-07-19 14:40:22 +0300

    amended js functions

--- index.js
+++ index.js
@@ -13,28 +13,25 @@ for (let i = 0; i < SQUARES_NUMBER; i += 1) {
     const square = document.createElement('div');
     square.classList.add('square');
 
-    square.addEventListener('mouseover', () => {
-        setSquareColor(square)
-    });
-    square.addEventListener('mouseleave', () => {
-        removeSquareColor(square)
-    });
+    square.addEventListener('mouseover', setSquareColor);
+    square.addEventListener('mouseleave', removeSquareColor);
 
     board.append(square);
 };
 
-function setSquareColor (element) {
+function setSquareColor (event) {
+    const element = event.target;
     const color = getRandomColor();
     element.style.backgroundColor = color;
     element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
 };
 
-function removeSquareColor (element) {
+function removeSquareColor (event) {
+    const element = event.target;
     element.style.backgroundColor = '#1d1d1d';
     element.style.boxShadow = '0 0 2px #000';
 };
 
 function getRandomColor () {
-    const index = Math.floor(Math.random() * colors.length);
-    return colors[index];
+    return colors[Math.floor(Math.random() * colors.length)];
 };
